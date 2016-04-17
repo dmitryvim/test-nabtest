579ced2f1b5d3f9280370b8c6de377eb56125e29
mhty <olivergrane@gmail.com>
2016-04-17 11:06:06 +0300

    values from properties

--- src/main/java/com/mikhaylovich/nabtest/ATSResource.java
+++ src/main/java/com/mikhaylovich/nabtest/ATSResource.java
@@ -1,5 +1,7 @@
 package com.mikhaylovich.nabtest;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,20 +10,38 @@ import javax.ws.rs.core.Response;
 
 @Path("/ats")
 public class ATSResource {
-//
-//  @Inject
-//  @Named("test.var")
-//  private String value;
+
+    /**
+     * Значения прописываются в файле settings.properties
+     */
+    @Inject
+    @Named("head.name")
+    private String head_name;
+
+    /**
+     * Значения прописываются в файле settings.properties
+     */
+    @Inject
+    @Named("head.phone")
+    private String head_phone;
+
 
 
     @GET
     @Path("/json")
     @Produces(value = "application/json")
-    public Response getStatus() {
+    public Response getJson() {
         Person person = new Person("Dima", "+79253550905");
         String personJson = JsonMapper.convertToJson(person);
         return Response.ok(personJson).build();
     }
 
-
+    @GET
+    @Path("/named")
+    @Produces(value = "application/json")
+    public Response getNamed() {
+        Person head = new Person(this.head_name, this.head_phone);
+        String personJson = JsonMapper.convertToJson(head);
+        return Response.ok(personJson).build();
+    }
 }
